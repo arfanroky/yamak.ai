38511d464078c7d79874ce0d83b916d8280aa8e6
Your Name <you@example.com>
2022-07-27 18:15:28 +0600

    changed the header

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
     <main className="mainContainer ">
       <div className="container mx-auto">
         <div className="d-flex justify-content-between align-items-center mb-3">
-          <h1 className="leftHeading fs-md1 fs-3">Home/Writing Assistant</h1>
+          <h1 className="leftHeading fs-md-1 fs-6">Home/Writing Assistant</h1>
           <div className="rightBtnGroup">
             <button className="button">Save</button>
             <button className="button d-md-inline d-none">Filter</button>
