879fdf80dda0eec57a176567f61311d98bcb2b2b
Your Name <you@example.com>
2022-07-27 18:01:49 +0600

    almost done the project with responsive

--- src/components/Assistant/Assistant.tsx
+++ src/components/Assistant/Assistant.tsx
@@ -1,26 +1,11 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React from 'react';
 
-function RowColLayoutExample() {
+const Assistant = () => {
   return (
-    <Container>
-      <Row xs={2} md={4} lg={6}>
-        <Col>1 of 2</Col>
-        <Col>2 of 2</Col>
-      </Row>
-      <Row xs={1} md={2}>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
-      </Row>
-      <Row xs="auto">
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
-      </Row>
-    </Container>
+    <div>
+      
+    </div>
   );
-}
+};
 
-export default RowColLayoutExample;
+export default Assistant;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,27 +4,77 @@ import React from 'react';
 import './Navbar.scss';
 
 const Navbar = () => {
-    return (
-        <nav className='navContainer'>
-            <div className='logo'>
-                <span className='blueColor'>Yamak</span>.ai
-            </div>
-            <ul className='navMenu'>
-                <li className='navItem'>Home</li>
-                <li className='navItem'>Marketing</li>
-                <li className='navItem blueColor'>Social Media</li>
-                <li className='navItem'>Sales Copy</li>
-                <li className='navItem'>Blogs</li>
-                <li className='navItem'>Others</li>
-                <li className='navItem badge'>
-                    
-                    <FontAwesomeIcon icon={faArrowsRotate}/>
-                    <span className='notification'>50</span>
-                    </li>
-                <li className='navItem user'><FontAwesomeIcon icon={faUser}/></li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="navContainer container-fluid">
+        
+      <div className="navMdContainer d-md-flex justify-content-between align-items-center d-none">
+        <div className="logo ">
+          <span className="blueColor">Yamak</span>.ai
+        </div>
+        <ul className="navMenu ">
+          <li className="navItem  ">Home</li>
+          <li className="navItem ">Marketing</li>
+          <li className="navItem blueColor  ">Social Media</li>
+          <li className="navItem  ">Sales Copy</li>
+          <li className="navItem ">Blogs</li>
+          <li className="navItem">Others</li>
+          <li className="navItem badge ">
+            <FontAwesomeIcon icon={faArrowsRotate} />
+            <span className="notification ">50</span>
+          </li>
+          <li className="navItem user ">
+            <FontAwesomeIcon icon={faUser} />
+          </li>
+        </ul>
+
+      </div>
+
+      <div className="navSmContainer d-md-none d-block">
+        <div className="navHeader">
+          <p className='fs-2 fw-bold'>
+            {' '}
+            <span className="blueColor">Yamak</span>.ai
+          </p>
+
+          <ul className="userActivity ">
+            <li className="notifyIcon">
+              <FontAwesomeIcon icon={faArrowsRotate} />
+              <span className="notification">50</span>
+            </li>
+            <li className="userIcon">
+              <FontAwesomeIcon icon={faUser} />
+            </li>
+          </ul>
+        </div>
+
+        <ul className="navMenu navSmMenu  fixed-bottom">
+          <li className="navItem fs-6 
+           me-2">Home</li>
+          <li className="navItem fs-6 
+           me-2">Marketing</li>
+          <li className="navItem blueColor  fs-6 
+           me-2">
+            Social Media
+          </li>
+          <li className="navItem  fs-6 
+           me-2">Sales Copy</li>
+          <li className="navItem  fs-6 
+           me-2">Blogs</li>
+          <li className="navItem fs-6 
+           me-2">Others</li>
+          <li className="navItem badge fs-6 
+           me-2">
+            <FontAwesomeIcon icon={faArrowsRotate} />
+            <span className="notification ">50</span>
+          </li>
+          <li className="navItem user fs-6 
+           me-2 ">
+            <FontAwesomeIcon icon={faUser} />
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
